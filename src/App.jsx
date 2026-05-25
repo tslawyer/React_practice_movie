ce04f32448e22ac0d3b7d6bbb78b479eafaf0594
Nick <melancholic8078@gmail.com>
2026-05-25 19:05:26 +0300

    updated for beter readability and added more composition

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,40 @@ import { useState } from "react";
 import { tempMovieData, tempWatchedData } from "./tempData";
 import NavComponent from "./components/NavComponent/NavComponent";
 import MainSection from "./components/MainSection/MainSection";
+import BoxComponent from './components/BoxComponent/BoxComponent'
+import WatchedMoviesList from './components/WatchedMoviesList/WatchedMoviesList'
+import MoviesListComponent from './components/MoviesListComponent/MoviesListComponent'
 
 function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+    const average = (arr) =>
+    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
 
   return (
     <>
       <NavComponent movies={movies} />
-      <MainSection watched={watched} movies={movies} />
+      <MainSection>
+        <BoxComponent>
+         <MoviesListComponent movies={movies} />
+        </BoxComponent>
+        <BoxComponent>
+           <WatchedMoviesList
+              watched={watched}
+              avgImdbRating={avgImdbRating}
+              avgUserRating={avgUserRating}
+              avgRuntime={avgRuntime}
+            />
+        </BoxComponent>
+
+      </MainSection>
     </>
   );
 }
--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,30 +1,9 @@
-import BoxComponent from "../BoxComponent/BoxComponent";
-import WatchedMoviesList from "../WatchedMoviesList/WatchedMoviesList";
-import MoviesListComponent from "../MoviesListComponent/MoviesListComponent";
+const MainSection = ({children}) => {
 
-const MainSection = ({ watched, movies }) => {
-  const average = (arr) =>
-    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
   return (
     <>
       <main className="main">
-        <BoxComponent children={<MoviesListComponent movies={movies} />} />
-        <BoxComponent
-          children={
-            <WatchedMoviesList
-              watched={watched}
-              avgImdbRating={avgImdbRating}
-              avgUserRating={avgUserRating}
-              avgRuntime={avgRuntime}
-            />
-          }
-        />
+       {children}
       </main>
     </>
   );
